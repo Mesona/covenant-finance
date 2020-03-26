88c1abc12cef6372854f66a2dfde15034e4ab86f
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-26 15:22:53 -0700

    Update cov-test.py

--- cov-test.py
+++ cov-test.py
@@ -1,8 +1,11 @@
 #! python3
 # cov-test.py - used to test functionality on covenant.py
 from covenant import Covenant
+import covenant_constants as cc
 
 cov = Covenant()
 print('Total covenant points: %s' % cov.calc_points())
 print('Needed servants and teamsters: %s' % cov.calc_needs())
+
+cov.bank(30)
 cov.display_finances()
