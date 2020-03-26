f6af19d691f769530236d83de96810d59f224332
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-26 15:22:41 -0700

    Update covenant.py
    add laboratory cost calculation

--- covenant.py
+++ covenant.py
@@ -1,20 +1,6 @@
 import math
+import covenant_constants as cc
 # Constants, pulled from the book.
-
-spring_calc = {
-'magi' : 5,
-'nobles' : 5,
-'companions' : 3,
-'crafters' : 2,
-'specialists': 2,
-}
-cost_max = {
-    'buildings' : 0.5,
-    'consumables' : 0.2,
-    'laboratories' : 0.2,
-    'provisions' : 0.2,
-    'arms' : 0.5,
-    'writing' : 0.5}
 
 class Covenant:
     def __init__(self):
@@ -33,15 +19,22 @@ class Covenant:
             'teamsters' : 7,
             'horses': 0
             }
-        self.covenfolk_points = self.calc_points()
-        self.expenditures = self.calc_expenditures()
+##        self.covenfolk_points = self.calc_points()
+##        self.expenditures = self.calc_expenditures()
+        self.laboratories = {
+            'Bonny' : 0,
+            'Merry' : 0,
+            'Tremmy' : 0,
+            'Jerry' : 0,
+            'Guenny' : 0,
+            'Ex Max' : 0}
         self.treasury = 50.0
 
     def calc_points(self):
         point_cost = 0
         if self.covenant_season == 'spring' or self.covenant_season == 'summer':
             for k, v in self.covenfolk_tiers.items():
-                point_cost += spring_calc.get(k, 1) * v
+                point_cost += cc.spring_calc.get(k, 1) * v
             #math for calculating
             return point_cost
         else:
@@ -53,7 +46,7 @@ class Covenant:
         points = 0
         if self.covenant_season == 'spring':
             for cov in cov_for_servants:
-                points += spring_calc.get(cov, 1) * self.covenfolk_tiers[cov]
+                points += cc.spring_calc.get(cov, 1) * self.covenfolk_tiers[cov]
         needs = [math.ceil(points / 10) * 2, math.ceil(points / 10) - (2 * self.covenfolk_tiers['laborers'])]
         return needs
     
@@ -62,22 +55,25 @@ class Covenant:
         expend['buildings'] = self.calc_points() / 10
         expend['consumables'] = 2 * (self.calc_points() / 10)
         expend['inflation'] = 0
-        expend['laboratories'] = 0
+        expend['laboratories'] = self.calc_labs() / 10
         expend['provisions'] = 5 * (self.calc_points() / 10)
+        expend['armory'] = 0
         expend['tithes'] = 0
         expend['wages'] = 2 * (self.calc_points() / 10)
+        expend['writing']= 0
         return expend
     
+    def calc_labs(self):
+        total = 0
+        for key, val in self.laboratories.items():
+            total += cc.lab_upkeep(val)
+        return total
+        
     def total_expenditure(self):
-        total = 0
-        for val in self.calc_expenditures().values():
-            total += val
-        return total
+        return sum(self.calc_expenditures().values())
+                   
     def total_income(self):
-        total = 0
-        for val in self.income_sources.values():
-            total += val
-        return total
+        return sum(self.income_sources.values())
 
     def display_finances(self):
         for key, val in self.calc_expenditures().items():
@@ -85,4 +81,14 @@ class Covenant:
         print ('Total:'.ljust(15) + str(self.total_expenditure()).rjust(8))
         print('\nTotal income:' + str(self.total_income()).rjust(10))
         print('Treasury:' + str(self.treasury).rjust(14))
+        #print()
+
+    def display_labs(self):
+        print('Name:'.ljust(15) + 'Upkeep'.rjust(8))
+        for key, val in self.laboratories.items():
+            print(key.ljust(20) + str(val).rjust(3))
+        #print()
     
+    def bank(self, silver):
+        self.treasury += silver
+
