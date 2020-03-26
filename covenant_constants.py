4b842c07ec393b8e8adb43ef6881bbdb5f2ceb96
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-26 15:21:43 -0700

    Create covenant_constants.py
    to keep track of constants and chats

---
+++ covenant_constants.py
@@ -0,0 +1,31 @@
+# covenant lab upkee
+
+#low upkeep costs
+lab_upkeep_low = {
+    -5: 1,
+    -4: 2,
+    -3: 3,
+    -2: 5,
+    -1: 7,
+    0 : 10,
+    1 : 15}
+def lab_upkeep(cost):
+    if cost < 2:
+        return lab_upkeep_low[cost]
+    return 5 * (cost) * (cost + 1)
+
+spring_calc = {
+'magi' : 5,
+'nobles' : 5,
+'companions' : 3,
+'crafters' : 2,
+'specialists': 2,
+}
+
+cost_max = {
+    'buildings' : 0.5,
+    'consumables' : 0.2,
+    'laboratories' : 0.2,
+    'provisions' : 0.2,
+    'arms' : 0.5,
+    'writing' : 0.5}
